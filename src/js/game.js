95154d0d37f2d79cbd4fba6e4358af9c2cda439d
jordi <1102000@hr.nl>
2025-05-06 16:06:54 +0200

    gamepieieiieieieieieiei

--- src/js/game.js
+++ src/js/game.js
@@ -1,13 +1,13 @@
 import '../css/style.css'
-import { Actor, Engine, Vector, DisplayMode } from "excalibur"
+import { Actor, Engine, Vector, DisplayMode, randomInRange } from "excalibur"
 import { Resources, ResourceLoader } from './resources.js'
 
 export class Game extends Engine {
 
     constructor() {
         super({ 
-            width: 1280,
-            height: 720,
+            width: 800,
+            height: 450,
             maxFps: 60,
             displayMode: DisplayMode.FitScreen
          })
@@ -16,12 +16,17 @@ export class Game extends Engine {
 
     startGame() {
         console.log("start de game!")
-        const fish = new Actor()
-        fish.graphics.use(Resources.Fish.toSprite())
-        fish.pos = new Vector(500, 300)
-        fish.vel = new Vector(-10,0)
-        fish.events.on("exitviewport", (e) => this.fishLeft(e))
-        this.add(fish)
+        const bg = new Actor()
+        bg.graphics.use(Resources.BG.toSprite())
+        this.add(bg)
+        for (let i = 0; i < 30; i++) {
+            const fish = new Actor()
+            fish.graphics.use(Resources.Fish.toSprite())
+            fish.pos = new Vector(randomInRange(0, 500), randomInRange(0, 300))
+            fish.vel = new Vector(randomInRange(-200, 0), randomInRange(0, 20))
+            fish.events.on("exitviewport", (e) => this.fishLeft(e))
+            this.add(fish)
+        }
     }
 
     fishLeft(e) {
